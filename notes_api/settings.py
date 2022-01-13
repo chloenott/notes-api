c8ca479a4e4a8c72746ef5b9be6a9b69ca34fc33
Chloe Nott <chloe@nott.email>
2022-01-12 22:50:39 -0800

    Dockerfile and docker-compose added. Functionality is good.

--- notes_api/settings.py
+++ notes_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9z-wu&4!*iqnqkbg^k&@8d%m-kis5raf^(@+acw^+0n=vy8ee+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 # Application definition
 
